cdf29e1e753f3b786b0d171908b733c21293586b
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-04-30 17:56:01 +0300

    решение задачи 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine("Да, первое число является квадратом второго и равно ");
+    Console.WriteLine(a);
+}
+else{
+    Console.WriteLine("Нет, квадрат второго числа не ровняется первому");
+}
