6eeed4a2f09783fab0a78c0c1fa8a3d74ebbe5ce
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-04-30 18:49:19 +0300

    решение задачи 3

---
+++ Задача3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0) Console.WriteLine("Это четное число");
+else if (a%2 != 0) Console.WriteLine("Это нечетное число");
