8de38c6ad62f86c4b7ce8d16ed2a3c66b5e5e275
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-04 21:29:35 +0300

    задача 5

---
+++ Задача5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+if (a>999 || a<100) Console.WriteLine("Это не трехзначное число");
+else 
+{
+    a = a/10%10;
+    Console.WriteLine($"Второе число 3х значного числа: {a}");
+}
+    
