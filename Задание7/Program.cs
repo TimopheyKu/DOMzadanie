96ef046c3523fc13d68c1ae7cf7f5d2935033084
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-05 20:18:53 +0300

    задача 7

--- Задание7/Program.cs
+++ Задание7/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Удалить вторую цифру трёхзначного числа
 Console.WriteLine("Введите 3х значное число");
 int a = int.Parse(Console.ReadLine());
-if (a>999 || a<100) Console.WriteLine("Это не трехзначное число");
+if (a>999 || a<100 && a<-999 || a > -99 ) Console.WriteLine("Это не трехзначное число");
 else
 {
     int b = int.Parse(a.ToString().Remove(1, 1));
