e4c0c88992af17742e40055db0aeb03b1823782b
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-05 21:45:45 +0300

    задача 9

---
+++ Задача9/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a < 99 && a > -99) Console.WriteLine("Третьей цифры числа нет. Введите другое число");
+else 
+{
+         string b = a.ToString();
+         Console.WriteLine($"Третья цефра числа = {b[2]}");
+}
