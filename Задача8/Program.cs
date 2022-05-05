fecf799deb23a7b0a65211a34948a1ef04a93c90
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-05 20:34:03 +0300

    задача 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите заданное число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите переменную b");
+int b = int.Parse(Console.ReadLine());
+if (a%b == 0)  Console.WriteLine($"Число {a} кратно числу {b} ");
+else Console.WriteLine($"{a%b}");
