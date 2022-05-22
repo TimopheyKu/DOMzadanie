60ad964ccb67443c913c975c164ee69de1c5235d
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 10:43:47 +0300

    урок 3, задание 1

---
+++ Урок3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите значение N");
+int N = int.Parse(Console.ReadLine());
+int start = 1;
+Console.WriteLine($"Таблица квадратов от 1 до {N}");
+while (start <= N)
+{
+    Console.WriteLine($"{start}|{start*start}");
+    start++;
+}
