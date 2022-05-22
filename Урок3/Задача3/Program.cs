797e0fd54770f07c0cca5389f9ef95f5e33a5b32
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 10:54:33 +0300

    урок 3, задание 3

---
+++ Урок3/Задача3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите значение А");
+int A = int.Parse(Console.ReadLine());
+int start = 1;
+int sum = 0;
+Console.WriteLine($"Сумма чисел от 1 до {A}");
+while (start <= A)
+{
+    sum = sum + start;
+    start++;
+}
+Console.WriteLine($"{sum}");
