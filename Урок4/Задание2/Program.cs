a9e670b979e55fca70d894114b1fd8938c06d58f
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 21:50:24 +0300

    урок 4, задание 2

---
+++ Урок4/Задание2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+int [] array = new int [12];
+Console.WriteLine("Задаем массив из 12 элементов");
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] =  new Random().Next(-9,9);
+    Console.WriteLine(array[i]);
+}
+int sumplus = 0;
+int summin = 0;
+
+for (int i = 0; i < array.Length; i++) 
+  if (array[i] < 0)
+    {
+        summin = summin + array[i];
+    }
+  else if (array[i] > 0)
+    {
+        sumplus = sumplus + array[i];
+    }
+
+Console.WriteLine($"Сумма положительных чисел = {sumplus}");
+Console.WriteLine($"Сумма отрицательных чисел = {summin}"); 
