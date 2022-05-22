cd230abef8ad2a9aa546526fc88687bcfcc1c2b0
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 13:02:11 +0300

    урок 3, задание 6

---
+++ Урок3/Задача6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0)
+{
+ a = a*a*a;
+ Console.WriteLine($"Куб числа а = {a}");
+}
+else Console.WriteLine("Число не заканчивается на четную цифру");
