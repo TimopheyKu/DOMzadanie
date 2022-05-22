c8068157eb2d113e2e7c713282742290310e7f80
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 21:50:59 +0300

    урок 4, задание 1

---
+++ Урок4/Задание1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] array = new int [8];
+Console.WriteLine("Массив из восеми чисел 0 и 1 в случайном порядке");
+for(int i = 0; i<array.Length; i++)
+{
+   array[i] =  new Random().Next(0,2);
+   Console.Write(array[i]);
+}
