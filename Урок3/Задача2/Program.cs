3e10f140b68554c206a56e7810d6a29bd30b9d65
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 10:47:20 +0300

    урок 3, задание 2

---
+++ Урок3/Задача2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите значение N");
+int N = int.Parse(Console.ReadLine());
+int start = 1;
+Console.WriteLine($"Кубы чисел от 1 до {N}");
+while (start <= N)
+{
+    Console.WriteLine($"{start}|{start*start*start}");
+    start++;
+}
