41f3de3882e8df0103c4492c45f7da4a2edabb31
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 12:06:21 +0300

    урок 3, задание 4

---
+++ Урок3/Задача4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите значение A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень В");
+int B = int.Parse(Console.ReadLine());
+int pow = A;
+int B1 = 1;
+while (B1 < B)
+{
+    pow = pow * A;
+    B1++;
+}
+
+Console.WriteLine($"Число {A} в натуральной степени {B} = {pow}");
