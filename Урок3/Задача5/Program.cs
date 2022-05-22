f723ea189cff9429dd3594d2fe14110ddb325f9e
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 12:43:03 +0300

    урок 3, задание 5

---
+++ Урок3/Задача5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+while (a > 0)
+{
+    sum = sum + a%10;
+    a = a/10;
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
