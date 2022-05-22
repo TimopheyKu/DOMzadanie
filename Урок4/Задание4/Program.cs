eaba99818bbb0cca0dde8056890a848902dc5d02
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-22 22:39:41 +0300

    урок 4, задание 4

---
+++ Урок4/Задание4/Program.cs
@@ -0,0 +1,17 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int [] array = new int [123];
+Console.WriteLine("Задаем массив из 20 элементов");
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,1000);
+    Console.WriteLine(array[i]);
+    if (array[i] > 10 && array[i] < 99)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine ($"Количество элементов из отрезка [10,99] в массиве = {sum}");
+
