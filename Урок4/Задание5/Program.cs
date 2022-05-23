b66066a80ed1fd61f12687a32fcdc50a0e97f0c5
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-23 10:29:54 +0300

    урок 4, задание 5

---
+++ Урок4/Задание5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int [] array = new int [5];
+Console.WriteLine("Задаем массив из 5 элементов");
+
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i]);
+}
