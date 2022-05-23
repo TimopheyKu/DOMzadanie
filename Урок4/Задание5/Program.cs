0776101f6e03c27c774b5337f1b206e739f02851
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-23 22:22:27 +0300

    урок 4, задание 5

--- Урок4/Задание5/Program.cs
+++ Урок4/Задание5/Program.cs
@@ -1,10 +1,24 @@
 ﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-int [] array = new int [5];
-Console.WriteLine("Задаем массив из 5 элементов");
-
+Console.WriteLine($"Введите количество элементов в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int [] array = new int [n];
+Console.WriteLine("Наш массив:"); 
 for (int i = 0; i < array.Length; i++) 
 {
     array[i] = new Random().Next(0,10);
-    Console.Write(array[i]);
+    Console.Write(array[i]); 
 }
+Console.WriteLine();
+
+int pow = 0;
+int size = array.Length;
+int last = array[size-1];
+ for (int i = 0; i < array.Length/2; i++)
+ {
+    pow = array[i]*array[size-1];
+    size--;
+    Console.WriteLine($"Произведение парных симметричных чисел равно = {pow}"); 
+ }
+
+
