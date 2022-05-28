35e385e721d07addef1ca1a45541ffa133b7eaca
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-05-28 17:32:15 +0300

    Задание 1, урок 5

---
+++ Урок5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением: 
+// y = k1 * x + b1, 
+// y = k2 * x + b2,        b1 k1 и b2 и k2 заданы   
+
+
+Console.WriteLine("Введите переменную b1");
+int b1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите переменную k1");
+int k1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите переменную b2");
+int b2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите переменную k2");
+int k2 = int.Parse(Console.ReadLine()); 
+
+int x = (b2 - b1)/(k1 - k2);
+int y = k2 * x + b2;
+if (k1 * x + b1 == k2 * x + b2 )
+{
+    Console.WriteLine($"Координаты точки пересечения: {x} and {y}");
+}
+else Console.WriteLine("Линии не пересекаются");
+
