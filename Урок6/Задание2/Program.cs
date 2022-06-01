e49c3a88f6c4fa30f125cbd1159d32145ee5a183
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-06-02 00:23:07 +0300

    Задание 2, урок 6

---
+++ Урок6/Задание2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n (Не совсем понял задание)
+Console.WriteLine("Введите числа n и m");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int [,] A = new int [m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        A[i,j] = new Random().Next(0,10);
+        Console.WriteLine(A[i,j]);
+    }
+    Console.WriteLine();
+}
