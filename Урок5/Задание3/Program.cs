d75fd6b0add31728612065ef13f71e3aaa0a573d
TimopheyKu <timon-kudryavcev@yandex.ru>
2022-06-01 10:49:50 +0300

    Задание 3, урок 5

---
+++ Урок5/Задание3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу копирования массива
+
+int[] array = new int[10];
+
+void PrintFillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,10);
+        Console.WriteLine(array[i]);
+    }
+}
+PrintFillArray(array);
+Console.WriteLine();
+void CopyArray (int [] array, int count)
+{
+    for (int j = 0; j < count; j++)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]  );
+        }
+        Console.WriteLine();
+    }
+    
+}
+Console.WriteLine("Задаем в функции колиство (count) копий нашего массива");
+CopyArray(array, 5);
